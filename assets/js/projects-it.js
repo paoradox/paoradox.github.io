7f84024be1503117e419cfabc6fc5705be438364
J.P Ancheta Javier <31607014+paoradox@users.noreply.github.com>
2026-09-10 22:58:42 +0800

    Added an IT project.

--- assets/js/projects-it.js
+++ assets/js/projects-it.js
@@ -113,6 +113,7 @@
 
   // --- Repo Data ---
   const repos = {
+    "JS-README-Builder": "README Builder: Markdown Editor App",
     "Python-Pynal-Destination": "Pynal Destination: Location History Recorder App",
     "Python-Video-Downloader": "Youtube Playlist Downloader",
     "QoL-Scripts": "Quality of Life (QoL) Scripts",
