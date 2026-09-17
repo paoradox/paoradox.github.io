8fefbbbf5d37fa6dc422529069ee8360fe83fbc5
J.P Ancheta Javier <31607014+paoradox@users.noreply.github.com>
2026-09-17 23:12:54 +0800

    Remove JS-README-Builder from repos list
    Removed 'JS-README-Builder' from the repository data.

--- assets/js/projects-it.js
+++ assets/js/projects-it.js
@@ -115,7 +115,6 @@
   const repos = {
     "JS-Portable-DTR-LAN": "DTR Manager: Portable DTR System", 
     "JS-Portable-Queue-LAN": "Queue Manager: Portable Queueing System", 
-    "JS-README-Builder": "README Builder: Markdown Editor App",
     "Python-Pynal-Destination": "Pynal Destination: Location History Recorder App",
     "Python-Video-Downloader": "Youtube Playlist Downloader",
     "QoL-Scripts": "Quality of Life (QoL) Scripts",
